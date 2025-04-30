859e5eee26699d2c163d7d005b7c652d0ae95b2f
Alisher3344 <alisher3344ali@gmail.com>
2025-04-30 07:52:36 +0500

    safsdfs

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -3,8 +3,14 @@ import React from "react";
 const First = () => {
   return (
     <div className="First_div">
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h2>Overview</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "40px",
+        }}
+      >
+        <h2 className="h2InFirst">Overview</h2>
         <div className="button_first">
           <button style={{ color: "rgba(157, 173, 185, 1)" }}>...</button>
           <button
@@ -19,111 +25,111 @@ const First = () => {
       </div>
       <div className="table">
         <div className="thead">
-          <p>Owner</p>
-          <p>End date</p>
-          <p>Profits</p>
-          <p>Losses</p>
-          <p>Phone</p>
+          <p className="pInTable">Owner</p>
+          <p className="pInTable">End date</p>
+          <p className="pInTable">Profits</p>
+          <p className="pInTable">Losses</p>
+          <p className="pInTable">Phone</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
             <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">1/15/12</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user2.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">8/30/14 </p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user3.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">5/30/14</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user4.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">11/7/16</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user5.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">7/27/13</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user6.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">5/19/12</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user7.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">8/21/15</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user8.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">5/27/15</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user9.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">3/4/16</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
         <div className="div_f">
-          <div style={{display:"flex",alignItems:"center",gap:"24px"}}> 
-            <img src="user1.svg" alt="" />
-            <h5>Savannah Nguyen</h5>
+          <div style={{ display: "flex", alignItems: "center", gap: "24px" }}>
+            <img src="user10.svg" alt="" />
+            <h5 className="pInTable">Savannah Nguyen</h5>
           </div>
-          <p>1/15/12</p>
+          <p className="pInTable">1/15/12</p>
           <p className="pls">$328.85</p>
           <p className="mns">$779.58</p>
-          <p>(603) 555-0123</p>
+          <p className="pInTable">(603) 555-0123</p>
         </div>
       </div>
     </div>
